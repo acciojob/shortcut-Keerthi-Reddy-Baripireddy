2f97d104dab89f89f2f74222664ebb59d548a5d2
Keerthi-Reddy-Baripireddy <144705049+Keerthi-Reddy-Baripireddy@users.noreply.github.com>
2023-11-14 15:06:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function shortcut(s1, s2) {
-  // your code here
+    // Check if either of the input strings is empty
+    if (s1.length === 0 || s2.length === 0) {
+        return '';
+    }
+
+    // Get the initial letters of the strings and convert to uppercase
+    const initialLetters = s1.charAt(0).toUpperCase() + s2.charAt(0).toUpperCase();
+
+    return initialLetters;
 }
 
 // Do not change the code below.
